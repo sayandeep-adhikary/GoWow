2d3215f98246bbd880cc5548ad4da325ef9994e2
sayandeep-adhikary <sayandeepadhikary2003@gmail.com>
2023-10-06 23:25:54 +0530

    final changes in opportunity card

--- src/components/Home/Opportunities/CardView/CardView.js
+++ src/components/Home/Opportunities/CardView/CardView.js
@@ -5,7 +5,7 @@ import OpportunityCard from "./OpportunityCard/OpportunityCard";
 
 export default function CardView() {
   return (
-    <div className="container"> 
+    <div className="container-fluid" style={{padding: '0 5rem'}}> 
       <div className="row">
         <div className="col-sm-12 col-lg-4 d-flex align-items-center justify-content-center">
           <OpportunityCard />
