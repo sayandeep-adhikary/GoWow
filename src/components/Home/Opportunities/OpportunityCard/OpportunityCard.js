2e09732dd570232e8dc8c28920963bf5a6229e35
sayandeep-adhikary <sayandeepadhikary2003@gmail.com>
2023-10-06 23:09:55 +0530

    add opportunity cards

--- src/components/Home/Opportunities/OpportunityCard/OpportunityCard.js
+++ src/components/Home/Opportunities/OpportunityCard/OpportunityCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./OpportunityCard.module.css";
 import {
   Avatar,
@@ -13,15 +13,14 @@ import {
   Image,
   Text,
 } from "@chakra-ui/react";
-import {SlLike} from 'react-icons/sl'
-import {BsChat} from 'react-icons/bs'
-import { BiLike, BiChat, BiShare } from "react-icons/bi";
+import { SlLike } from "react-icons/sl";
+import { BsChat } from "react-icons/bs";
 import NGOLogo from "../../../../assets/NGOLogo.png";
 import opportunityImg from "../../../../assets/opportunityImg.png";
 
 export default function OpportunityCard() {
   return (
-    <Card maxW="lg" my={5} borderRadius={"2xl"} fontSize={"0.8rem"}>
+    <Card maxW="lg" my={5} borderRadius={"2xl"} fontSize={"0.8rem"} boxShadow={'0 0 5px grey'}>
       <CardHeader pb={0} px={3}>
         <Flex spacing="4">
           <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
@@ -46,24 +45,62 @@ export default function OpportunityCard() {
         Prabhu Agya Foundation is organizing blood donation camp.
       </Text>
 
-      <CardFooter justify="space-between" py={2} flex>
+      <CardFooter justify="space-between" py={5} flex>
         <Box display={"flex"} gap={5}>
-          <Flex px={0} alignItems={'center'} gap={2} fontWeight={'600'}>
-            <SlLike size={20} color="#5B4899" cursor={'pointer'} />
+          <Flex px={0} alignItems={"center"} gap={2} fontWeight={"600"}>
+            <SlLike size={20} color="#5B4899" cursor={"pointer"}/>
             <Text mb={0}>4.5K</Text>
           </Flex>
-          <Flex px={0} alignItems={'center'} gap={2} fontWeight={'600'}>
-            <BsChat size={20} color="#5B4899" cursor={'pointer'} />
+          <Flex px={0} alignItems={"center"} gap={2} fontWeight={"600"}>
+            <BsChat size={20} color="#5B4899" cursor={"pointer"} />
             <Text mb={0}>150</Text>
           </Flex>
         </Box>
-        <Box>
-          <Button px={0} variant="ghost">
-            Share
+        <Box display={"flex"} gap={5}>
+          <Button
+            color={"color.1"}
+            _hover={{
+              color: "white",
+              bg: "color.1",
+            }}
+            variant={"outline"}
+            style={{ letterSpacing: "1px", border: "1px solid #5B4899" }}
+            h={8}
+            w={"4rem"}
+          >
+            <Text
+              fontFamily={"'Poppins', sans-serif"}
+              mb={0}
+              letterSpacing={"1px"}
+              fontWeight={600}
+              fontSize={"0.8rem"}
+            >
+              Share
+            </Text>
           </Button>
-          {/* <Button flex="1" variant="ghost" leftIcon={<BiChat />}>
-            Comment
-          </Button> */}
+          <Button
+            color={"white"}
+            bg={"color.1"}
+            _hover={{
+              bg: "white",
+              border: "1px solid #5B4899",
+              color: "#5B4899",
+            }}
+            variant={"outline"}
+            style={{ letterSpacing: "1px" }}
+            h={8}
+            w={"4rem"}
+          >
+            <Text
+              fontFamily={"'Poppins', sans-serif"}
+              mb={0}
+              letterSpacing={"1px"}
+              fontWeight={600}
+              fontSize={"0.8rem"}
+            >
+              Grab
+            </Text>
+          </Button>
         </Box>
       </CardFooter>
     </Card>
